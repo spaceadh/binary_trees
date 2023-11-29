410f3a5eef76c569895281d8410a01c7b741f10d
spaceadh <spacesealadhesives13@gmail.com>
2023-11-29 07:13:37 -0800

    DoneFortheDay🥱😭

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		if (tree)
+		{
+			left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return ((left > right) ? left : right);
+	}
+}
+
