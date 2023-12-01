b55b86f180017fa78ebd9a1291f61f156816e3ca
omotomiwa26 <omotomiwa26@gmail.com>
2023-12-01 00:03:33 +0000

    5 main file

--- 5-main.c
+++ 5-main.c
@@ -1,12 +1,11 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
     binary_tree_t *root;
