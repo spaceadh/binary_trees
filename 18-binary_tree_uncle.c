fcc37846f0594b0fe73ec76899a46a7e558d7571
omotomiwa26 <omotomiwa26@gmail.com>
2023-12-01 21:47:03 +0000

    function that finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,18 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - function that finds if a node is sibling
- * @node: node to check
- * Return: The uncle node
+ * binary_tree_uncle - function finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ * Return: Pointer to uncle node
+ * Else: NULL
  */
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *grandparent;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
-	if (node->parent->parent->left == node->parent)
-		return (node->parent->parent->right);
-	return (node->parent->parent->left);
+
+	grandparent = node->parent->parent;
+
+	if (grandparent->left == node->parent)
+	{
+		return (grandparent->right);
+	}
+	else if (grandparent->right == node->parent)
+	{
+		return (grandparent->left);
+	}
+
+	return (NULL);
 }
